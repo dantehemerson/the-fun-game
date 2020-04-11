882ed12ae667ab0ce567f083621e68feb6599e22
Dante Calderon <mail@dantecalderon.dev>
2020-04-10 22:18:33 -0500

    Remove shit code

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,37 +1,4 @@
 import * as React from 'react'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
-
-import './index.css'
-
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
 
 interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
   location: {
@@ -44,24 +11,7 @@ class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
   public render() {
     return (
       <div>
-        <Helmet
-          title="Gatsby Default Starter"
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        />
-        <Header />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {this.props.children()}
-        </div>
+        {this.props.children()}
       </div>
     )
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react'
-import Link from 'gatsby-link'
 
-// Please note that you can use https://github.com/dotansimha/graphql-code-generator
-// to generate all types from graphQL schema
 interface IndexPageProps {
   data: {
     site: {
@@ -13,23 +10,12 @@ interface IndexPageProps {
   }
 }
 
-export default class extends React.Component<IndexPageProps, {}> {
-  constructor(props: IndexPageProps, context: any) {
-    super(props, context)
-  }
-  public render() {
+const Index = (props: IndexPageProps) => {
     return (
       <div>
-        <h1>Hi people</h1>
-        <p>
-          Welcome to your new{' '}
-          <strong>{this.props.data.site.siteMetadata.title}</strong> site.
-        </p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
+        Hola
       </div>
     )
-  }
 }
 
 export const pageQuery = graphql`
@@ -41,3 +27,4 @@ export const pageQuery = graphql`
     }
   }
 `
+export default Index
